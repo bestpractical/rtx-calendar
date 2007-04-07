8f3d46c17e661776a2fe2fe6fc424de5f7d04755
Nicolas Chuche <nicolas@barna.be>
2007-04-07 01:23:02 +0000

    * change doc * change css * better date form * cosmetics changes

--- lib/RTx/Calendar.pm
+++ lib/RTx/Calendar.pm
@@ -1,37 +1,38 @@
 package RTx::Calendar;
 
 use strict;
+use Time::Local;
 
 our $VERSION = "0.01";
 
 sub FirstMonday {
-	my $date = shift;
-	$date->Set(Format => 'unix', Value => Time::Local::timelocal( 0,0,3,1,(localtime $date->Unix)[4..5] ) );
-	# search previous monday
-	my $wday = (localtime($date->Unix))[6];
-	$date->Set(Format => 'unix', Value => $date->Unix - (($wday - 1) % 7) * 24 * 60 * 60);
-	$date;
+    my $date = shift;
+    $date->Set(Format => 'unix', Value => Time::Local::timelocal( 0,0,3,1,(localtime $date->Unix)[4..5] ) );
+    # search previous monday
+    my $wday = (localtime($date->Unix))[6];
+    $date->Set(Format => 'unix', Value => $date->Unix - (($wday - 1) % 7) * 24 * 60 * 60);
+    $date;
 }
 
-sub LastSunday {
-	my $date = shift;
-	my ($month,$year) = (localtime($date->Unix))[4,5];
-	++$month; $year += 1900;
-	if ($month > 11) {
-		$month = 0; $year++;
-        }
-	$date->Set(Format => 'unix', Value => Time::Local::timelocal( 0,0,3,1,$month,$year) );
-	my $wday = (localtime($date->Unix))[6];
-	$date->Set(Format => 'unix', Value => $date->Unix + ((0 - $wday) % 7 + 1) * 24 * 60 * 60);
-	$date;
+sub NextSunday {
+    my $date = shift;
+    my ($month,$year) = (localtime($date->Unix))[4,5];
+    ++$month; $year += 1900;
+    if ($month > 11) {
+	$month = 0; $year++;
+    }
+    $date->Set(Format => 'unix', Value => Time::Local::timelocal( 0,0,3,1,$month,$year) );
+    my $wday = (localtime($date->Unix))[6];
+    $date->Set(Format => 'unix', Value => $date->Unix + ((0 - $wday) % 7 + 1) * 24 * 60 * 60);
+    $date;
 }
 
-sub FormatDate {
-        my $date = shift;
-	my @ym = (split /-/,$date->Date )[0,1];
-        my $return = $date->GetMonth($ym[1] - 1) . " $ym[0]";
+# wrong in many case
+sub WeekNumber {
+    my $timestamp = shift;
+    my $yearday = (localtime $timestamp)[7];
+    return my $weeknum = int( $yearday / 7 ) + 1;
 }
-
 
 1;
 
@@ -48,7 +49,10 @@ This document describes version 0.01 of RTx::Calendar, released
 
 =head1 DESCRIPTION
 
-This RT extension provides a calendar view for your tickets
+This RT extension provides a calendar view for your tickets. You can
+find it in the menu Tools->Calendar.
+
+Idea borrowed from redmine's calendar (Thanks Jean-Philippe).
 
 =head1 AUTHORS
 
