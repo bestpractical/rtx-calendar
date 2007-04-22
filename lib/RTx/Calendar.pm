0646973bfef67c3a793c121ffc74d4e07c85930d
Nicolas Chuche <nicolas@barna.be>
2007-04-22 13:03:59 +0000

    add some doc

--- lib/RTx/Calendar.pm
+++ lib/RTx/Calendar.pm
@@ -53,15 +53,15 @@ This document describes version 0.03 of RTx::Calendar
 
 =head1 DESCRIPTION
 
-This RT extension provides a calendar view for your tickets so you see
-when is your next due ticket. You can find it in the menu
-Search->Calendar.
+This RT extension provides a calendar view for your tickets and your
+reminders so you see when is your next due ticket. You can find it in
+the menu Search->Calendar.
 
-There's a portlet to put on your own page (see Prefs/MyRT.html)
+There's a portlet to put on your home page (see Prefs/MyRT.html)
 
-You can also enable ics (ICal) feeds for all your private searches in
-Prefs/Calendar.html. Authentication is magic number based so that you
-can give those feeds to other people.
+You can also enable ics (ICal) feeds for your default calendar and all
+your private searches in Prefs/Calendar.html. Authentication is magic
+number based so that you can give those feeds to other people.
 
 You can find screenshots on
 http://gaspard.mine.nu/dotclear/index.php?tag/rtx-calendar
@@ -85,12 +85,18 @@ $HomepageComponents in etc/RT_SiteConfig.pm like that :
 To enable private searches ICal feeds, you need to give
 CreateSavedSearch and LoadSavedSearch rights to your users.
 
+=head1 ADVANCED USAGE
+
+If you want to see reminders in a search you need to go in the
+"advanced" tab of the query builder and add something like that :
+
+  AND ( Type = 'ticket' OR Type = 'reminder' )
+
 =head1 UPGRADE FROM 0.02
 
-As I've change directory structure and Calendar now use local, if you
-upgrade from 0.02 you need to delete old files manually. Go in
-RTHOME/share/html (by default /opt/rt3/share/html) and delete those
-files :
+As I've change directory structure, if you upgrade from 0.02 you need
+to delete old files manually. Go in RTHOME/share/html (by default
+/opt/rt3/share/html) and delete those files :
 
   rm -rf Callbacks/RTx-Calendar
   rm NoAuth/images/arrow*.png
@@ -99,6 +105,17 @@ files :
   rm Tools/Calendar.html
 
 RTx-Calendar may work without this but it's not very clean.
+
+=head1 BUGS
+
+=over
+
+=item *
+compatible only with RT 3.6 for the moment. If someone need
+compatibility with 3.4 I can work on this. And I will work on 3.7
+compatibility later.
+
+=back
 
 =head1 AUTHORS
 
