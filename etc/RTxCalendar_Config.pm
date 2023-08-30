4996150ceee6b5c1192535855ba3628ee5f39300
Ronaldo Richieri <ronaldo@bestpractical.com>
2023-08-29 21:26:35 -0300

    Add feature to customize color of events on the calendar

--- etc/RTxCalendar_Config.pm
+++ etc/RTxCalendar_Config.pm
@@ -1,3 +1,11 @@
+Set(%CalendarStatusColorMap, (
+    'new'                                   => '#87873c',
+    'open'                                  => '#5555f8',
+    'rejected'                              => '#FF0000',
+    'resolved'                              => '#72b872',
+    'stalled'                               => '#FF0000',
+));
+
 Set($CalendarSortEvents, sub {
     my @Tickets = @_;
     my @SortedTickets = sort { lc($a->Status) cmp lc($b->Status) } @Tickets;
