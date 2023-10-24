0c1ff33d6412ca2eaf9de92bced5ddc54d5fa6c1
Ronaldo Richieri <ronaldo@bestpractical.com>
2023-10-24 16:10:55 -0400

    Add default config

---
+++ etc/RTxCalendar_Config.pm
@@ -0,0 +1,25 @@
+Set(%CalendarIcons, (
+    'Reminder'     => 'reminder.png',
+    'Resolved'     => 'resolved.png',
+    'Starts, Due'  => 'starts_due.png',
+    'Created, Due' => 'created_due.png',
+    'Created'      => 'created.png',
+    'Due'          => 'due.png',
+    'Starts'       => 'starts.png',
+    'Started'      => 'started.png',
+    'LastUpdated'  => 'updated.png',
+));
+
+Set(%CalendarStatusColorMap, (
+    'new'                                   => '#87873c',
+    'open'                                  => '#5555f8',
+    'rejected'                              => '#FF0000',
+    'resolved'                              => '#72b872',
+    'stalled'                               => '#FF0000',
+));
+
+Set(@CalendarFilterStatuses, qw(new open stalled rejected resolved));
+
+Set(@CalendarFilterDefaultStatuses, qw(new open));
+
+1;
